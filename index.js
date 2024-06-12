85a33e11cdf8dc46ff17000563a01df0a48c3893
MamidwarSampada <s19_mamidwar_sampada@mgmcen.ac.in>
2024-06-12 15:17:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+
+function showlearning(){
+    document.getElementById('learning_img').style.display='block';
+    document.getElementById('technology_img').style.display='none';
+    document.getElementById('communication_img').style.display='none';
+    document.getElementById('learning_info').style.display='block';
+    document.getElementById('technology_info').style.display='none';
+    document.getElementById('communication_info').style.display='none';
+}
+function showtechnology(){
+    document.getElementById('technology_img').style.display='block';
+    document.getElementById('learning_img').style.display='none';
+    document.getElementById('communication_img').style.display='none';
+    document.getElementById('technology_info').style.display='block';
+    document.getElementById('learning_info').style.display='none';
+    document.getElementById('communication_info').style.display='none';
+  
+}
+
+function showcommunication(){
+    document.getElementById('communication_img').style.display='block';
+    document.getElementById('learning_img').style.display='none';
+    document.getElementById('technology_img').style.display='none';
+    document.getElementById('communication_info').style.display='block';
+    document.getElementById('learning_info').style.display='none';
+    document.getElementById('technology_info').style.display='none';
+   
+}
